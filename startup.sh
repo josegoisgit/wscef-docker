aa4eaf88db750a2423a6a2885367e9a3c9698597
José Gois <josegoismail@gmail.com>
2021-07-09 16:28:02 +0000

    Update startup.sh
    Banco do Brasil customized options and URL

--- startup.sh
+++ startup.sh
@@ -9,4 +9,4 @@ runx xauth add ${HOSTNAME}/unix${DISPLAY} . $(runx xauth list | awk '$1 !~ /loca
 runx xauth generate $DISPLAY . untrusted timeout 0
 
 /usr/local/bin/warsaw/core \
-&& runx firefox -no-remote -private-window --class CaixaEconomica --name CaixaEconomica https://www.caixa.gov.br
+&& runx firefox -no-remote -private-window --class BB --name BB https://www.bancobrasil.com.br/aapf/login.html
